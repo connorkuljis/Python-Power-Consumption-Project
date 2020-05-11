7be1e4c16ce17321376b008d59f406006b3f6c19
ConcoDev <conkuljis@gmail.com>
2020-05-11 19:26:18 +0800

    found daily change and fixed error in csv, plotted two subplots

--- Inves1/powerusage.py
+++ Inves1/powerusage.py
@@ -11,14 +11,30 @@ dates = []
 with open('powerusage.csv', 'r') as f:
     lines = f.readlines()
 
+# cleaning csv and sorting into lists
 for line in lines:
     s_lines = line.strip().split(',')
     powerusage.append(int(s_lines[0]))
     dates.append(s_lines[1])
 
+# just get the daily change
+dailyusage = [j - i for i, j in zip(powerusage[: -1], powerusage[1 :])]
+
+print("Daily usage:", dailyusage)
 print("Power usage: ", powerusage)
 print("Date: ", dates)
 
+plt.figure(1)
+plt.subplot(211)
+plt.title("Cumulative Power Usage Over Time")
+plt.xlabel("Date")
+plt.ylabel("Energy Usage (kWh)")
+plt.plot(dates, powerusage, 'r-')
 
-plt.plot(dates, powerusage)
+plt.subplot(212)
+plt.title("Daily Power Usage Over Time")
+plt.xlabel("Date")
+plt.ylabel("Energy Usage (kWh)")
+plt.plot(dates[1:], dailyusage, 'b.')
+
 plt.show()
